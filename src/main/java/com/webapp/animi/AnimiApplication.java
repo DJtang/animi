1ce851ab9a1b53b2fbe10894649e1e49b207920c
Cat like fish <tangfeng.email@qq.com>
2023-03-27 20:37:34 +0800

    初始化

---
+++ src/main/java/com/webapp/animi/AnimiApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.animi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/animi/AnimiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.animi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
